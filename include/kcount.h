b1a56ec2be6e53b6f3b22b8e1f5418010ca366ed
gf777 <giulio.formenti@gmail.com>
2024-06-16 00:26:45 -0400

    added key typename

--- include/kcount.h
+++ include/kcount.h
@@ -1,7 +1,7 @@
 #ifndef KCOUNT_H
 #define KCOUNT_H
 
-class KDB : public Kmap<KDB, UserInputKcount, uint8_t, uint32_t> { // CRTP
+class KDB : public Kmap<KDB, UserInputKcount, uint64_t, uint8_t, uint32_t> { // CRTP
     UserInputKcount userInput;
 public:
     KDB(UserInputKcount& userInput) : Kmap{userInput}, userInput(userInput) {
